d748c387fdbf4fc8b7ab99f4a7d5bbd2ee3f8412
azaarifin <bcs24090059@student.uts.edu.my>
2025-04-29 22:25:58 +0800

    home.jsx

---
+++ frontend/src/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AirQualityPolling from './AirQualityPolling';
+import WaterQualityPolling from './WaterQualityPolling';
+
+function Home() {
+  return (
+    <div>
+      <h1>Sarawak Environmental Monitoring System</h1>
+      <p>Real-time air and water quality monitoring</p>
+      <AirQualityPolling />
+      <WaterQualityPolling />
+    </div>
+  );
+}
+
+export default Home;
