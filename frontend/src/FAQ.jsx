779118cc5b98b96fd2df5706635e9762e23ad8c7
azaarifin <bcs24090059@student.uts.edu.my>
2025-05-14 15:38:12 +0800

    Update FAQ.jsx

--- frontend/src/FAQ.jsx
+++ frontend/src/FAQ.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 
 function FAQ() {
   return (
-    <div>
+    <div className="glass-box">
       <h2>Frequently Asked Questions</h2>
       <ul>
-        <li><strong>Q:</strong> Where is the data from?<br /><strong>A:</strong> Data is monitored in Sibu, Sarawak in real-time.</li>
-        <li><strong>Q:</strong> What is WQI?<br /><strong>A:</strong> Water Quality Index — it measures water cleanliness.</li>
+        <li>
+          <strong>Q:</strong> Where is the data from?<br />
+          <strong>A:</strong> Data is monitored in Sibu, Sarawak in real-time.
+        </li>
+        <li>
+          <strong>Q:</strong> What is WQI?<br />
+          <strong>A:</strong> Water Quality Index — it measures water cleanliness.
+        </li>
+        <li>
+          <strong>Q:</strong> Where did you get the WQI?<br />
+          <strong>A:</strong>  the WQI data is a dummy data (no source, just representation of how its calculated and monitored)
+        </li>
       </ul>
     </div>
   );
